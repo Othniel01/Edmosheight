8da429c875e1357fe8991bd0afb04ca308ef0828
othni <othnielabalaka@gmail.com>
2023-03-08 10:47:25 +0100

    updated scroll to top

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const navbar = document.getElementById("navigation");
+const scrollToTop = document.getElementById("top-btn");
 let scrolled = false;
 
 window.onscroll = function () {
   if (window.pageYOffset > 100) {
     navbar.classList.remove("top");
+    scrollToTop.classList.remove("topbtn-gone");
     if (!scrolled) {
       navbar.style.transform = "translateY(-70px)";
+      scrollToTop.style.transform = "translateY(30px)";
     }
     setTimeout(function () {
       navbar.style.transform = "translateY(0)";
+      scrollToTop.style.transform = "translateY(0)";
       scrolled = true;
     }, 200);
   } else {
     navbar.classList.add("top");
+    scrollToTop.classList.add("topbtn-gone");
     scrolled = false;
   }
 };
@@ -22,6 +27,21 @@ window.onscroll = function () {
 $("#showcase a").on("click", function (e) {
   if (this.hash !== "") {
     e.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
+});
+
+$("#top.btn").on("click", function (r) {
+  if (this.hash !== "") {
+    r.preventDefault();
 
     const hash = this.hash;
 
